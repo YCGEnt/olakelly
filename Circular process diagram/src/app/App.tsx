154f8dc4d9c8abc75756706c970f9bb70ff1dbb9
olaiyakelly-netizen <olaiyakelly@gmail.com>
2026-03-15 13:42:45 -0400

    Add files via upload
    added figma diagram

---
+++ Circular process diagram/src/app/App.tsx
@@ -0,0 +1,9 @@
+import { StewardFramework } from './components/StewardFramework';
+
+export default function App() {
+  return (
+    <div className="size-full" style={{ backgroundColor: '#0f1629' }}>
+      <StewardFramework />
+    </div>
+  );
+}
